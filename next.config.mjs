d237aed42b0c646fe2dc88c6f5383f8e343d6429
Viet <nguyenkieuhoangviet@gmail.com>
2024-11-18 23:26:26 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   basePath: "/timnguyenus",
   output: "export",
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["github.com"],
     path: "https://hoangviet2796.github.io/timnguyenus",
