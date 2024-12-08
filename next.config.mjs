d79ec86e68c2e2138ad680f993868c37a6a7626d
Viet <nguyenkieuhoangviet@gmail.com>
2024-12-09 00:11:49 +0700

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
+import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/timnguyenus",
-  output: "export",
-  reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    loader: "custom",
-    loaderFile: "./loader.js",
-  },
+  // basePath: "/timnguyenus",
+  // output: "export",
+  // reactStrictMode: true,
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  // images: {
+  //   loader: "custom",
+  //   loaderFile: "./loader.js",
+  // },
 };
 
-export default nextConfig;
+export default withNextVideo(nextConfig);
