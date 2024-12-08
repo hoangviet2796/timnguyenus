027c33e0754d60e7732cb78e6d34cfb4396b405d
Viet Nguyen <87941341+hoangviet2796@users.noreply.github.com>
2024-12-08 23:37:44 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,19 @@ const nextConfig = {
     loader: "custom",
     loaderFile: "./loader.js",
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac|rtf|doc|docx)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[hash].[ext]',
+          publicPath: '/_next/',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
