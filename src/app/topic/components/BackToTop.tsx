2c015737ff147e542b7041243fdd444d5fc9a8df
Nguyễn Quốc Hùng <nguyenhung016446@gmai.com>
2024-12-29 15:02:09 +0700

    update CSS

--- src/app/topic/components/BackToTop.tsx
+++ src/app/topic/components/BackToTop.tsx
@@ -1,48 +1,25 @@
-"use client";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function BackToTopButton() {
-  const [isNearBottom, setIsNearBottom] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.innerHeight + window.scrollY; 
-      const pageHeight = document.documentElement.scrollHeight; 
-      const threshold = 150; 
-
-      if (pageHeight - scrollPosition <= threshold) {
-        setIsNearBottom(true);
-      } else {
-        setIsNearBottom(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <>
-      {isNearBottom && (
-        <Link
-          href="/topic"
-          className="fixed bottom-[430px] right-4 bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-800 transition duration-300 z-50"
-          aria-label="Quay lại"
+    <div className="flex justify-center mt-12">
+      <Link
+        href="/topic"
+        className="bg-blue-500 text-white rounded-full px-4 py-2 shadow-lg hover:bg-blue-600 transition duration-300 hover:underline"
+        aria-label="Quay lại"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="w-6 h-6 inline-block mr-2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </Link>
-      )}
-    </>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+        Xem Tất Cả Bài Viết
+      </Link>
+    </div>
   );
 }
