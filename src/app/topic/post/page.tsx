4c199c2235c09cd9e5a9899170e39be4b7593368
Viet <nguyenkieuhoangviet@gmail.com>
2024-12-29 15:45:37 +0700

    update

--- src/app/topic/post/page.tsx
+++ src/app/topic/post/page.tsx
@@ -31,37 +31,44 @@ export default function PostDetail() {
     return <h1>{error}</h1>;
   }
   if (!post) {
-    return
+    return;
   }
   return (
     <div className="max-w-[1280px] mx-auto py-12  box-border ">
-  {/* Thông tin tác giả */}
-  <div className="flex items-center text-sm text-gray-600 mb-6 space-x-2">
-    {/* Ảnh đại diện */}
-    <Image
-      src={post.authorImage}
-      alt={post.authorName}
-      width={40}
-      height={40}
-      className="rounded-full object-cover"
-    />
-    {/* Thông tin chi tiết */}
-    <span className="font-medium text-sm text-gray-700">{post.authorName}</span>
-    <span>•</span>
-    <span className="font-medium text-sm text-gray-700">{post.date}</span>
-    <span>•</span>
-    <span className="font-medium text-sm text-gray-700">{post.timeToRead}</span>
-  </div>
-  {/* Tiêu đề bài viết */}
-  <h1 className="text-3xl font-bold text-gray-800 mb-6 leading-relaxed">{post.title}</h1>
-  {/* Phần giới thiệu */}
-  <p className="text-lg font-bol text-black mb-8 leading-8">{post.introduction}</p>
-  {/* Nội dung */}
-  <PostContent content={post.content} conclusion={post.conclusion} />
-   
-   {/* Nút quay lại */}
-   <BackToTopButton />   
-</div>
+      {/* Thông tin tác giả */}
+      <div className="flex items-center text-sm text-gray-600 mb-6 space-x-2">
+        {/* Ảnh đại diện */}
+        <Image
+          src={post.authorImage}
+          alt={post.authorName}
+          width={40}
+          height={40}
+          className="rounded-full object-cover"
+        />
+        {/* Thông tin chi tiết */}
+        <span className="font-medium text-sm text-gray-700">
+          {post.authorName}
+        </span>
+        <span>•</span>
+        <span className="font-medium text-sm text-gray-700">{post.date}</span>
+        <span>•</span>
+        <span className="font-medium text-sm text-gray-700">
+          {post.timeToRead}
+        </span>
+      </div>
+      {/* Tiêu đề bài viết */}
+      <h1 className="text-3xl font-bold text-gray-800 mb-6 leading-relaxed">
+        {post.title}
+      </h1>
+      {/* Phần giới thiệu */}
+      <p className="text-lg font-bol text-black mb-8 leading-8">
+        {post.introduction}
+      </p>
+      {/* Nội dung */}
+      <PostContent content={post.content} conclusion={post.conclusion} />
 
+      {/* Nút quay lại */}
+      <BackToTopButton />
+    </div>
   );
 }
