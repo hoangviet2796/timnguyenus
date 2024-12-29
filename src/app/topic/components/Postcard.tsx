685caf7c76be8a38407cf024f3407458be8e0de8
Viet <nguyenkieuhoangviet@gmail.com>
2024-12-29 16:19:04 +0700

    update

--- src/app/topic/components/Postcard.tsx
+++ src/app/topic/components/Postcard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 type PostCardProps = {
   id: string;
   title: string;
@@ -21,47 +22,54 @@ export default function PostCard({
   authorImage,
 }: PostCardProps) {
   return (
-    <div className="w-[460px] overflow-hidden shadow-md bg-white flex flex-col mx-auto transition-transform duration-300">
-      {/* Hình ảnh bài viết */}
-      <Link href={`/topic/post?id=${id}`} >
-        <div className="relative w-full h-[400px] cursor-pointer overflow-hidden  ">
-          <Image src={image} alt="Post image" fill className="object-cover transition-transform duration-300 ease-in-out hover:scale-110" />
-        </div>
-      </Link>
-      
-      <div className="p-6">
-        {/* Thông tin tác giả */}
-        <div className="flex items-center space-x-4 mb-1">
-          <Image
-            src={authorImage}
-            alt={authorName}
-            width={40}
-            height={40}
-            className="rounded-full object-cover"
-          />
-          <div className="text-sm">
-            <p className=" text-[14px] text-gray-800">{authorName}</p>
-            <p className="text-gray-500 mt-1 text-xs italic">
-              {date} - {timeToRead}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="px-6 pb-6 group ">
-        {/* Tiêu đề bài viết */}
+    <Suspense>
+      <div className="w-[460px] overflow-hidden shadow-md bg-white flex flex-col mx-auto transition-transform duration-300">
+        {/* Hình ảnh bài viết */}
         <Link href={`/topic/post?id=${id}`}>
-          <h2 className="text-[25px] font-bold text-gray-800  cursor-pointer group-hover:text-[#917237]">
-            {title}
-          </h2>
+          <div className="relative w-full h-[400px] cursor-pointer overflow-hidden  ">
+            <Image
+              src={image}
+              alt="Post image"
+              fill
+              className="object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
         </Link>
 
-        {/* Mô tả bài viết */}
-        <Link href={`/topic/post?id=${id}`}>
-          <p className="text-gray-600 mt-4 text-[16px] cursor-pointer group-hover:text-[#917237]  ">
-            {introduction.split(" ").slice(0, 34  ).join(" ")}...
-          </p>
-        </Link>
+        <div className="p-6">
+          {/* Thông tin tác giả */}
+          <div className="flex items-center space-x-4 mb-1">
+            <Image
+              src={authorImage}
+              alt={authorName}
+              width={40}
+              height={40}
+              className="rounded-full object-cover"
+            />
+            <div className="text-sm">
+              <p className=" text-[14px] text-gray-800">{authorName}</p>
+              <p className="text-gray-500 mt-1 text-xs italic">
+                {date} - {timeToRead}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 pb-6 group ">
+          {/* Tiêu đề bài viết */}
+          <Link href={`/topic/post?id=${id}`}>
+            <h2 className="text-[25px] font-bold text-gray-800  cursor-pointer group-hover:text-[#917237]">
+              {title}
+            </h2>
+          </Link>
+
+          {/* Mô tả bài viết */}
+          <Link href={`/topic/post?id=${id}`}>
+            <p className="text-gray-600 mt-4 text-[16px] cursor-pointer group-hover:text-[#917237]  ">
+              {introduction.split(" ").slice(0, 34).join(" ")}...
+            </p>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
--- src/app/topic/post/page.tsx
+++ src/app/topic/post/page.tsx
@@ -2,7 +2,6 @@
 import BackToTopButton from "../components/BackToTop";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Suspense } from "react";
 import Image from "next/image";
 import PostContent from "../components/PostConten";
 export default function PostDetail() {
@@ -35,43 +34,41 @@ export default function PostDetail() {
     return;
   }
   return (
-    <Suspense>
-      <div className="max-w-[1280px] mx-auto py-12  box-border ">
-        {/* Thông tin tác giả */}
-        <div className="flex items-center text-sm text-gray-600 mb-6 space-x-2">
-          {/* Ảnh đại diện */}
-          <Image
-            src={post.authorImage}
-            alt={post.authorName}
-            width={40}
-            height={40}
-            className="rounded-full object-cover"
-          />
-          {/* Thông tin chi tiết */}
-          <span className="font-medium text-sm text-gray-700">
-            {post.authorName}
-          </span>
-          <span>•</span>
-          <span className="font-medium text-sm text-gray-700">{post.date}</span>
-          <span>•</span>
-          <span className="font-medium text-sm text-gray-700">
-            {post.timeToRead}
-          </span>
-        </div>
-        {/* Tiêu đề bài viết */}
-        <h1 className="text-3xl font-bold text-gray-800 mb-6 leading-relaxed">
-          {post.title}
-        </h1>
-        {/* Phần giới thiệu */}
-        <p className="text-lg font-bol text-black mb-8 leading-8">
-          {post.introduction}
-        </p>
-        {/* Nội dung */}
-        <PostContent content={post.content} conclusion={post.conclusion} />
-
-        {/* Nút quay lại */}
-        <BackToTopButton />
+    <div className="max-w-[1280px] mx-auto py-12  box-border ">
+      {/* Thông tin tác giả */}
+      <div className="flex items-center text-sm text-gray-600 mb-6 space-x-2">
+        {/* Ảnh đại diện */}
+        <Image
+          src={post.authorImage}
+          alt={post.authorName}
+          width={40}
+          height={40}
+          className="rounded-full object-cover"
+        />
+        {/* Thông tin chi tiết */}
+        <span className="font-medium text-sm text-gray-700">
+          {post.authorName}
+        </span>
+        <span>•</span>
+        <span className="font-medium text-sm text-gray-700">{post.date}</span>
+        <span>•</span>
+        <span className="font-medium text-sm text-gray-700">
+          {post.timeToRead}
+        </span>
       </div>
-    </Suspense>
+      {/* Tiêu đề bài viết */}
+      <h1 className="text-3xl font-bold text-gray-800 mb-6 leading-relaxed">
+        {post.title}
+      </h1>
+      {/* Phần giới thiệu */}
+      <p className="text-lg font-bol text-black mb-8 leading-8">
+        {post.introduction}
+      </p>
+      {/* Nội dung */}
+      <PostContent content={post.content} conclusion={post.conclusion} />
+
+      {/* Nút quay lại */}
+      <BackToTopButton />
+    </div>
   );
 }
