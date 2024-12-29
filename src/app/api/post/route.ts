9e8e1f6de1360f70909e33849eddafafd9ed1a9b
Nguyễn Quốc Hùng <nguyenhung016446@gmai.com>
2024-12-29 23:13:44 +0700

    Update route.ts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -13,7 +13,7 @@ export async function GET(req: Request) {
     }
 
     // Fetch file JSON từ public
-    const baseUrl =  new URL(req.url).origin || ""; 
+    const baseUrl =  new URL(req.url).origin || "http://localhost:3001"; 
     const response = await fetch(`${baseUrl}/data.json`);
 
     if (!response.ok) {
