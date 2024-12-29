5a98866a8f00d18b8f5ff6f5513ea582a6015307
Viet <nguyenkieuhoangviet@gmail.com>
2024-12-29 17:07:20 +0700

    update

---
+++ src/app/program/page.tsx
@@ -0,0 +1,34 @@
+import Main from "@/app/home/main/main";
+import VideoHome from "@/app/(content)/about/videoHome/videoHome";
+import FeedBack from "@/app/home/feedback/feedback";
+import Cooporate from "@/app/home/cooperate/Cooperate";
+import Advice from "@/app/home/advise/advise";
+import FeedbackClient from "@/app/home/feedbackClient/feedbackClient";
+import Review from "@/app/home/review/review";
+import GoodDeal from "@/app/home/goodeal/gooddeal";
+import Referral from "@/app/home/referral/referral";
+import Tool from "@/app/home/tools/tool";
+import Theory from "@/app/home//theory/theory";
+import FormInfo from "@/app/(form)/formInfo";
+
+export default function program() {
+  return (
+    <main className={`flex flex-col items-center cursor-default`}>
+      {/* <div className={`max-w-screen-xl cursor-default`}> */}
+      <Main />
+      {/* <VideoHome /> */}
+      <Theory />
+      <FeedBack />
+      <Tool />
+      <Cooporate />
+      {/* <Advice /> */}
+      <FeedbackClient />
+      <Review />
+      <GoodDeal />
+      <Referral />
+      {/* <Contact /> */}
+      {/* </div> */}
+      <FormInfo />
+    </main>
+  );
+}
