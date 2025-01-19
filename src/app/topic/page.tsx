a074b80600b19a9d121207351677cc3339e5acfe
Viet <nguyenkieuhoangviet@gmail.com>
2025-01-20 00:04:41 +0700

    update

--- src/app/topic/page.tsx
+++ src/app/topic/page.tsx
@@ -1,15 +1,15 @@
-// import fs from "fs";
-// import path from "path";
-// import PostList from "./components/PostList";
+import fs from "fs";
+import path from "path";
+import PostList from "./components/PostList";
 
-// export default async function TopicPage() {
-//   const filePath = path.join(process.cwd(), "public", "data.json"); // Đường dẫn tới file trong thư mục public
-//   const jsonData = fs.readFileSync(filePath, "utf-8");
-//   const posts = JSON.parse(jsonData);
+export default async function TopicPage() {
+  const filePath = path.join(process.cwd(), "public", "data.json"); // Đường dẫn tới file trong thư mục public
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const posts = JSON.parse(jsonData);
 
-//   return (
-//     <div style={{ padding: "60px", background: "#f9f9f9" }}>
-//       <PostList posts={posts} />
-//     </div>
-//   );
-// }
+  return (
+    <div style={{ padding: "60px", background: "#f9f9f9" }}>
+      {/* <PostList posts={posts} /> */}
+    </div>
+  );
+}
